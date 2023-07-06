2b92c99b00d2baf4f712f8f23e00475d1ac03755
MarianaKely <marianakely.r@gmail.com>
2023-07-06 20:42:35 -0300

    feat: schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,5 @@
 export * from './users-schemas';
 export * from './authentication-schemas';
 export * from './enrollments-schemas';
+export * from './payment-schema';
+export * from './tickets-schema'
