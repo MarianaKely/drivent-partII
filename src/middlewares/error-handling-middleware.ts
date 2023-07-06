bd218203584caecf6656a57cddbfbf906361829a
MarianaKely <marianakely.r@gmail.com>
2023-07-06 20:43:33 -0300

    feat: update error middleware

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -38,6 +38,17 @@ export function handleApplicationErrors(
     });
   }
 
+
+  if (err.name === 'PaymentError') {
+    return res.status(httpStatus.PAYMENT_REQUIRED).send({ message: err.message,});
+  }
+  
+
+  if (err.name === 'InvalidDataError') {
+    return res.status(httpStatus.BAD_REQUEST).send({ message: err.message, });
+  }
+
+
   /* eslint-disable-next-line no-console */
   console.error(err.name);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
